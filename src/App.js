5b1b2849752f2252ab5ef6969fb21cb0a6c5539c
Jake Le <lejake888@gmail.com>
2019-11-18 17:04:41 +0000

    React calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    numbers : 
+    [
+      {zero: 0},
+      {one: 1},
+      {two: 2},
+      {three: 3},
+      {four: 4},
+      {five: 5},
+      {six: 6},
+      {seven: 7},
+      {eight: 8},
+      {nine: 9},
+    ],
+    screen: [],
+    total: 0
+}
+
+  ClearPress = () => {
+    this.setState({screen: []})
+  }
+
+  NumberPress = (number) => {
+    const list = this.state.screen.concat(number);
+    this.setState({screen: list})
+    // console.log(this.state.screen)
+    return {
+      list,
+      value: '',
+    }
+  }
+  
+  // OperatorPress = (operator) => {
+  //   const list = this.state.screen.concat(operator);
+  //   this.setState({screen: list})
+  //   return {
+  //     list,
+  //     value: '',
+  //   }
+  // }
+
+  render () {
+    return(
+      <div className = "calculator">
+        <div className = "screen">
+          <p>{this.state.screen}</p>
+        </div>
+          <div className = "allButtons"> 
+              <div className = "mainButtons">
+                <button id = "zero" onClick = {() => this.NumberPress(0)}>0</button>
+                <button id = "one" onClick = {() => this.NumberPress(1)}>1</button>
+                <button id = "two" onClick = {() => this.NumberPress(2)}>2</button>
+                <button id = "three" onClick = {() => this.NumberPress(3)}>3</button>
+                <button id = "four" onClick = {() => this.NumberPress(4)}>4</button>
+                <button id = "five" onClick = {() => this.NumberPress(5)}>5</button>
+                <button id = "six" onClick = {() => this.NumberPress(6)}>6</button>
+                <button id = "seven" onClick = {() => this.NumberPress(7)}>7</button>
+                <button id = "eight" onClick = {() => this.NumberPress(8)}>8</button>
+                <button id = "nine" onClick = {() => this.NumberPress(9)}>9</button>
+                <button id = "clear" onClick = {() => this.ClearPress()}>Clear</button>
+              </div>
+              <div className = "operatorButtons">
+                <button id = "divide" onClick = {() => this.NumberPress("/")}>÷</button>
+                <button id = "multiply" onClick = {() => this.NumberPress("*")}>*</button>
+                <button id = "minus" onClick = {() => this.NumberPress("-")}>-</button>
+                <button id = "add" onClick = {() => this.NumberPress("+")}>+</button>
+                <button id = "equal" onClick = {() => this.NumberPress("=")}>=</button>
+              </div>
+          </div>
+      </div>
+      )
+  }
 }
 
 export default App;
